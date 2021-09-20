3cdf976566cd5aeb4dd93532cb39afb066676df7
Senne <vanrompaey.senne@protonmail.com>
2021-09-20 11:58:30 +0200

    Reset powershell scripts

--- tools/patchinstaller.ps1
+++ tools/patchinstaller.ps1
@@ -5,19 +5,16 @@ param(
 )
 
 $productVersionToken = '<?define ProductVersion = "0.0.0"?>'
-$productIdToken = '<?define ProductId = "{}"?>'
-$upgradeCodeToken = '<?define UpgradeCode = "{}">'
+$productIdToken = '<?define ProductId = "{CE7B5507-834E-4C1C-85D7-15598EBC52C4}"?>'
 
 $newVersion = "$major.$minor.$patch"
 Write-Host "Updating version to $newVersion"
 
 $newGuid = [guid]::NewGuid()
-$upgradeCodeGuid = [guid]::NewGuid()
 Write-Host "Creating new GUID $newGuid"
 
 $targetFile = $PSScriptRoot + '/../src/AudioBandInstaller/Product.wxs'
 $versionReplace = "<?define ProductVersion = '$newVersion'?>"
 $guidReplace = "<?define ProductId = '$newGuid'?>"
-$upgradeCodeReplace = "<?define UpgradeCode = '$upgradeCodeGuid'?>"
 
-(Get-Content "$targetFile").replace("$productVersionToken", "$versionReplace").replace("$productIdToken", "$guidReplace").replace("$upgradeCodeToken", "$upgradeCodeReplace") | Set-Content -Encoding UTF8 "$targetFile"
+(Get-Content "$targetFile").replace("$productVersionToken", "$versionReplace").replace("$productIdToken", "$guidReplace") | Set-Content -Encoding UTF8 "$targetFile"
