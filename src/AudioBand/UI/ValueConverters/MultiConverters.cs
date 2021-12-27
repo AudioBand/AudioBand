ff4ad8017439aa670860c4c8e8642a8ad5607dca
Senne <vanrompaey.senne@protonmail.com>
2021-12-27 22:45:40 +0100

    Remove SharpVector dependency

--- src/AudioBand/UI/ValueConverters/MultiConverters.cs
+++ src/AudioBand/UI/ValueConverters/MultiConverters.cs
@@ -19,10 +19,5 @@
         /// Gets a <see cref="PointConverter"/>.
         /// </summary>
         public static PointConverter Point { get; } = new PointConverter();
-
-        /// <summary>
-        /// Gets a <see cref="PathToImageSource"/> multi converter.
-        /// </summary>
-        public static PathToImageSourceConverter PathToImageSource { get; } = new PathToImageSourceConverter();
     }
 }
