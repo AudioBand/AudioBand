312bad1af13883c0f2ecdd29010959ac2633f88d
Senne <vanrompaey.senne@protonmail.com>
2021-12-27 23:39:01 +0100

    Update .net framework

--- src/AudioBand/GlobalSettings.Designer.cs
+++ src/AudioBand/GlobalSettings.Designer.cs
@@ -12,7 +12,7 @@ namespace AudioBand {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class GlobalSettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static GlobalSettings defaultInstance = ((GlobalSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new GlobalSettings())));
