433788ba1f32d0ef0dd810f8872fae30c1d9dc28
TechSupport26 <zephd26iscool@gmail.com>
2022-07-05 23:54:19 +1000

    AddGradientProgressBar

--- src/AudioBand/Deskband.cs
+++ src/AudioBand/Deskband.cs
@@ -42,6 +42,7 @@ namespace AudioBand
         /// </summary>
         public Deskband()
         {
+            System.Diagnostics.Debugger.Launch();
             // Fluentwpf requires an application window
             if (Application.Current == null)
             {
